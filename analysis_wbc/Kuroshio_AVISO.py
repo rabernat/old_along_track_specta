afdf761d1d8126084972f64ac52e961fa9d67626
Ryan Abernathey <rpa@ldeo.columbia.edu>
2014-07-10 23:47:26 -0400

    working

--- analysis_wbc/Kuroshio_AVISO.py
+++ analysis_wbc/Kuroshio_AVISO.py
@@ -1,26 +1,15 @@
 from pylab import *
 import os
 from scipy.io import netcdf
-import mpl_toolkits.basemap as bm
 import pyresample
+from datetime import datetime, timedelta
+from scipy.signal import periodogram
 
-data_dir = '/Volumes/Bucket1/Data/AVISO/global/delayed-time/along-track/filtered/sla/j2'
-years = range(2008,2013)
+# which data set to use
+dset = 'filtered'; fsuffix = 'vfec'; Nx=160; DX=14e3 
+#dset = 'unfiltered'; fsuffix = 'vxxc'; Nx=320; DX=7e3
 
-# the grid we used before
-area_name = 'Kuroshio Orthographic Grid'
-proj_id = 'ease_ortho'
-area_id = proj_id
-#proj4_args = '+proj=laea +lat_0=32 +lon_0=172 +a=6371228.0 +units=m'
-proj4_args = '+proj=ortho +lat_0=32 +lon_0=172 +units=m'
-x_size = 400
-y_size = 400
-area_extent = (-1600000.,-1600000.,1600000.,1600000.)
-area_def = pyresample.utils.get_area_def(area_id, area_name, proj_id, proj4_args,
-                                       x_size, y_size, area_extent)
-
-#my_track_ids = [34,110,186,8,84,160,236,58,135,210,032,108,
-#             227,49,125,201,23,99,175,251,73,149,255,47,123,199]
+data_dir = '/Volumes/Bucket1/Data/AVISO/global/delayed-time/along-track/%s/sla/j2' % dset
 
 my_track_ids = [ 75,151,227, 49,125,201, 23, 99,175,251, 73,149,225, 47,123,199, 21, 97,173,249,
                 234,158, 82,  6,184,108, 32,210,134, 58,236,160, 84,  8,186,110, 34,212,136]
@@ -31,79 +20,114 @@ lon_min, lon_max = 150.,180.
 
 min_track_length=40
 
-# the spacing of SLA points
-DX = 14e3 
-# the largest possible track length
-Nx = 160
+
 # size of the wavenumber grid
 Nk = Nx/2
-K = fftfreq(Nx, 14e3)
+K = fftfreq(Nx, DX)
+DK = K[1] - K[0]
 
 track_count = dict()
 
-Nt = 36*5             
-fulltime,t,count = 0, 0, 0
+start_date = datetime(2008,10,19)
+end_date = datetime(2012,12,31)
+
 DT = 10 # number of days to average over             
+Nt = (end_date - start_date).days / 10 
+fulltime,t,count = 0, 0, 0
+dates = [(start_date + timedelta(days=(5+n*DT)))
+         for n in range(Nt)]
+dateo = array([(start_date + timedelta(days=(5+n*DT))).toordinal()
+         for n in range(Nt)])
 
-sla_ft = zeros((Nt,Nx))
-sla_ft_tmp = zeros(Nx)
+sla_ps = zeros((Nt,Nx))
+sla_ps_tmp = zeros(Nx)
 
-for y in years:
-    for f in os.listdir(os.path.join(data_dir, '%4d' % y)):
-        try:
-            nc = netcdf.netcdf_file(os.path.join(data_dir, '%4d' % y, f))
-            lonvar = nc.variables['longitude']
-            latvar = nc.variables['latitude']
-            slavar = nc.variables['SLA']
-            fulltime += 1
-            print f
-        except IOerror:
-            break
-        if (mod(fulltime,DT)==0):
-            print 'Resetting'
-            sla_ft[t] = sla_ft_tmp / count
-            sla_ft_tmp = 0
-            count = 0
-            t += 1
-        track = nc.variables['track'][:]
-        # exclude the first and last track
-        tdiff = find(diff(track)!=0)
-        bnd_left = hstack([0,tdiff+1])
-        bnd_right = hstack([tdiff+1, len(track)])
-        Ntracks = len(bnd_left)
-        track_ids = track[bnd_left]
-        for n in range(Ntracks):
-            tid = track_ids[n]
-            if tid in my_track_ids:
-                lon = lonvar[bnd_left[n]:bnd_right[n]]*lonvar.scale_factor
-                lat = latvar[bnd_left[n]:bnd_right[n]]*latvar.scale_factor
-                sla = slavar.scale_factor * slavar[bnd_left[n]:bnd_right[n]]
-                idx = (lon>=lon_min) & (lon<=lon_max) & (lat>=lat_min) & (lat<=lat_max)
-                if sum(idx) > min_track_length:
-                    sla = sla[idx]
-                    #sla_spec = sla_wavenumber_power_spectrum(lon, lat, sla)
+this_date = start_date
+while this_date <= end_date:
+    y = this_date.year
+    # get this day's file
+    f = 'dt_global_j2_sla_%s_%s_20140106.nc' % (fsuffix, this_date.strftime('%Y%m%d'))
+    fname = os.path.join(data_dir, '%4d' % y, f)
+    try:
+        nc = netcdf.netcdf_file(fname)
+        lonvar = nc.variables['longitude']
+        latvar = nc.variables['latitude']
+        slavar = nc.variables['SLA']
+        fulltime += 1
+        print f
+    except IOerror:
+        print 'No file found for %s' % this_date 
+    if (mod(fulltime,DT)==0):
+        print 'Resetting'
+        sla_ps[t] = sla_ps_tmp / count
+        sla_ps_tmp = 0
+        count = 0
+        t += 1
+    track = nc.variables['track'][:]
+    # find track boundaries
+    tdiff = find(diff(track)!=0)
+    bnd_left = hstack([0,tdiff+1])
+    bnd_right = hstack([tdiff+1, len(track)])
+    Ntracks = len(bnd_left)
+    track_ids = track[bnd_left]
+    for n in range(Ntracks):
+        tid = track_ids[n]
+        if tid in my_track_ids:
+            lon = lonvar[bnd_left[n]:bnd_right[n]]*lonvar.scale_factor
+            lat = latvar[bnd_left[n]:bnd_right[n]]*latvar.scale_factor
+            sla = slavar.scale_factor * slavar[bnd_left[n]:bnd_right[n]]
+            idx = (lon>=lon_min) & (lon<=lon_max) & (lat>=lat_min) & (lat<=lat_max)
+            if sum(idx) > min_track_length:
+                sla = sla[idx]
+                #sla_spec = sla_wavenumber_power_spectrum(lon, lat, sla)
+                if True:
                     sla -= sla.mean()
                     sla *= hanning(len(sla))
-                    sla_ft_tmp += abs(fft(sla,Nx))
-                    count += 1
-                    try:
-                        track_count[tid].append(len(sla))
-                    except KeyError:
-                        track_count[tid] = [len(sla),]
+                    sla_fft = fft(sla,Nx)
+                    # power spectrum
+                    ps = real(sla_fft * sla_fft.conj()) / len(sla)
+                    # check parseval's theorem
+                    msla2 = mean(sla**2)
+                    assert abs(msla2 - mean(ps))/msla2 < 1e-10
+                    sla_ps_tmp += ps
+                else:
+                    freq,sla_ps_tmp = periodogram(sla, DX,
+                        window='hanning', nfft=Nx, detrend='linear',
+                        scaling='density', return_onesided=False)
+                count += 1
+                try:
+                    track_count[tid].append(len(sla))
+                except KeyError:
+                    track_count[tid] = [len(sla),]
+    this_date += timedelta(days=1)
+
+# normalize the power spectrum to be cm^2 / cycles / km
+# (instead of m^2 / cycles / m)
+sla_ps *= 0.1
+
 
 T = arange(Nt)*DT/365.*12.        
-idx_winter = mod(T-3,12)<2
-idx_summer = mod(T-9,12)<1
+idx_winter = array([ d.month==1 for d in dates ])
+idx_summer = array([ d.month==7 for d in dates ])
 
-mean_spectrum_summer = (sla_ft**2)[idx_summer].mean(axis=0)
-mean_spectrum_winter = (sla_ft**2)[idx_winter].mean(axis=0)
+mean_spectrum_summer = sla_ps[idx_summer].mean(axis=0)
+mean_spectrum_winter = sla_ps[idx_winter].mean(axis=0)
 
 figure()
 loglog(K[:Nk]*1e3, mean_spectrum_summer[:Nk])
 loglog(K[:Nk]*1e3, mean_spectrum_winter[:Nk])
-loglog(K[:Nk][10:30]*1e3, 1e-8*(K[:Nk][10:30]*1e3)**-(11/3.), 'k--')
+#loglog(K[:Nk][10:25]*1e3, 5e-8*(K[:Nk][10:25]*1e3)**-(11/3.), 'k--')
+#loglog(K[:Nk][8:20]*1e3, 1e-12*(K[:Nk][8:20]*1e3)**-5, 'k-')
 
-EKE_ft = K**2*sla_ft**2
+legend(['Summmer','Winter',r'$K^{-11/3}$'])
+xlabel('K (cpkm)'); ylabel('Power')
+title('SLA Power Spectrum : %s' % dset)
+
+EKE_ft = K**2*sla_ps
 figure()
-pcolormesh(K[1:Nk]*1e3,T,ma.masked_invalid(log10(EKE_ft[:,1:Nk])))
-        
+pcolormesh(K[1:Nk]*1e3,dateo,ma.masked_invalid(log10(EKE_ft[:,1:Nk])))
+#xlim([250**-1, 70**-1])
+#gca().set_xscale('log')
+gca().yaxis_date()
+xlabel('K (cpkm)')
+title('SLA Power Spectrum : %s' % dset)
